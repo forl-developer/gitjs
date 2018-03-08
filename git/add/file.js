82b6f6f936bcdabab3b342e00a3c62a45d5c2f28
kentaro <kentaro@rcnp.osaka-u.ac.jp>
2018-03-08 18:53:53 +0900

    Add git/add module

---
+++ git/add/file.js
@@ -0,0 +1,19 @@
+const exec=require('child_process').execSync;
+const isPrivateKey=require('../../isPrivateKey');
+
+const file=(filepath)=>{
+    if( filepath==null ) return false;
+    if( isPrivateKey(filepath) ){ throw new Error('!!!!! '+filepath+' is private key !!!!!'); }
+    
+
+    try{ exec('git add '+filepath); }
+    catch(e){
+	console.log('git.add.file catch exception');
+	if( e.stdout.toString().length>0 ){ console.log('std out : ', e.stdout.toString()); }
+	console.log('std err : ', e.stderr.toString());
+	return false;
+    }
+    return true;
+}
+
+module.exports=file;
